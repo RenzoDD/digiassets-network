946522a7870ff56014329b83b0656996dd8ad23a
Renzo Diaz <renzo.diaz@remadi.net>
2021-11-11 05:09:59 -0500

    Added DigiAsset model

--- db/03-digi-assets.sql
+++ db/03-digi-assets.sql
@@ -29,6 +29,24 @@ BEGIN
 	LIMIT   1;
 END //
 
+DROP PROCEDURE IF EXISTS DigiAssets_Read_DigiAssetID //
+CREATE PROCEDURE DigiAssets_Read_DigiAssetID ( IN DigiAssetID INTEGER )
+BEGIN
+    SELECT  COUNT(*) AS Quantity
+    FROM    DigiAssets AS DA
+    WHERE   DA.DigiAssetID = DigiAssetID
+    LIMIT   1;
+END //
+
+DROP PROCEDURE IF EXISTS DigiAssets_Read_AssetID //
+CREATE PROCEDURE DigiAssets_Read_AssetID ( IN AssetID VARCHAR(64) )
+BEGIN
+    SELECT  COUNT(*) AS Quantity
+    FROM    DigiAssets AS DA
+    WHERE   DA.AssetID = AssetID
+    LIMIT   1;
+END //
+
 DROP PROCEDURE IF EXISTS DigiAssets_Read_Quantity //
 CREATE PROCEDURE DigiAssets_Read_Quantity ( )
 BEGIN
